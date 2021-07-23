45b6c0e0c9cfd50bb3ea1b9e47a09924a10a1810
Natália Nuñez <natalia.nunez@sumup.com>
2021-07-23 11:10:44 -0300

    Add user schema

---
+++ priv/repo/migrations/20210723140934_create_users.exs
@@ -0,0 +1,13 @@
+defmodule ProjectOne.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :age, :string
+
+      timestamps()
+    end
+
+  end
+end
